92b6ba1ab2535beba86ce81c547ae24fe6bed9ea
OmerUygurOzer <omer.u.ozer@gmail.com>
2016-07-12 22:12:12 -0600

    created the application class

---
+++ app/src/main/java/com/boomer/omer/kollabstr/KollabstrApplication.java
@@ -0,0 +1,14 @@
+package com.boomer.omer.kollabstr;
+
+import android.app.Application;
+
+/**
+ * Created by Omer on 7/12/2016.
+ */
+public class KollabstrApplication extends Application {
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+    }
+}
